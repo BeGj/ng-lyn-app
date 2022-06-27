80202c1cb93b208b4e7a8039162212686fced5fb
psc007 <sikk1996@gmail.com>
2022-06-27 16:37:48 +0200

    webgl and history

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Map, View } from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import { scan, } from 'rxjs';
+import { Feature, Map, View } from 'ol';
+import { concatMap, map, scan, shareReplay, startWith, withLatestFrom, } from 'rxjs';
 import { LynMetService } from './services/lyn-met.service';
 import { OSM, XYZ } from "ol/source";
 import Tile from 'ol/layer/Tile';
 import { fromLonLat } from 'ol/proj';
+import Vector from 'ol/source/Vector';
+import WebGLPointsLayer from 'ol/layer/WebGLPoints';
+import { Point } from "ol/geom";
+import { LiteralStyle } from 'ol/style/literal';
+import TileLayer from 'ol/layer/WebGLTile';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +17,46 @@ import { fromLonLat } from 'ol/proj';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent  implements OnInit{
-  lyn$ = this.lynMet.getServerSentEvent().pipe(
+  timeNow = new Date().getTime();
+  lyn$ = this.lynMet.getServerSentEvent().pipe(shareReplay());
+  lynFromLast10Min$ = this.lynMet.getLynHistory(10).pipe(shareReplay());
+  accumulatedLynnedslag$ = this.lynFromLast10Min$.pipe(
+    concatMap(history => this.lyn$.pipe(map(lyn => {
+      return [...history, ...lyn]
+    }))),
     scan((acc, curr) => [...acc, ...curr]),
   );
   private olMap!: Map
+  private glStyle: LiteralStyle = {
+    "symbol": {
+      "symbolType": "circle",
+      "size": 10,
+      "color": [
+        "interpolate",
+        [
+          "linear"
+        ],
+        [
+          "get",
+          "time"
+        ],
+        this.timeNow - 600_000,
+        "#ff6a19",
+        this.timeNow,
+        "#5aca5b"
+      ],
+      "rotateWithView": true
+    }
+  };
+  private lynLayer = new WebGLPointsLayer({
+    source: new Vector<Point>({
+      features: []
+    }),
+    style: this.glStyle,
+    disableHitDetection: true,
+  });
+
+
 
   constructor(
     private lynMet: LynMetService
@@ -28,7 +68,8 @@ export class AppComponent  implements OnInit{
       layers: [
         new Tile({
           source: new OSM()
-        })
+        }),
+        this.lynLayer
       ],
       view: new View({
         center: fromLonLat([20.5651,68.7628]),
@@ -41,7 +82,29 @@ export class AppComponent  implements OnInit{
         center: fromLonLat([position.coords.longitude, position.coords.latitude]),
         zoom: 10
       });
-    })
+    });
+    this.lyn$.subscribe(lynEvent => {
+      this.lynLayer.getSource()?.addFeatures(
+        lynEvent.map(lyn => {
+          const point = new Point(fromLonLat(lyn.Point))
+          return new Feature({
+            geometry: point
+          });
+        })
+      )
+    });
+    this.lynFromLast10Min$.subscribe(lynEvent => {
+      this.lynLayer.getSource()?.addFeatures(
+        lynEvent.map(lyn => {
+          const point = new Point(fromLonLat(lyn.Point))
+          const feature =  new Feature({
+            geometry: point
+          });
+          feature.set('time',lyn.Epoch.getTime())
+          return feature;
+        })
+      )
+    });
 
   }
 }
--- src/app/services/lyn-met.service.ts
+++ src/app/services/lyn-met.service.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { LynEvent } from '../models/lyn-event.model';
 
 @Injectable({
@@ -7,7 +8,7 @@ import { LynEvent } from '../models/lyn-event.model';
 })
 export class LynMetService {
   protected eventStreamUrl = 'https://lyn.met.no/events';
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   getServerSentEvent(): Observable<LynEvent[]> {
     return new Observable<LynEvent[]>(subscriber => {
@@ -19,10 +20,9 @@ export class LynMetService {
         const message = e.data as string;
         const splitMessage =  message.split(/\r?\n/);
         console.log(splitMessage);
-        const datas = splitMessage.map(data => JSON.parse(data) as LynEvent[] )
-
+        const datas = splitMessage.map(data => JSON.parse(data) as LynEvent[])
         subscriber.next(
-          datas.flat()
+          datas.flat().map(lyn => ({...lyn,Epoch: new Date(lyn.Epoch)}))
         );
       })
 
@@ -35,5 +35,23 @@ export class LynMetService {
     });
   }
 
+  getLynHistory(minutes: number): Observable<LynEvent[]> {
+    const sec = minutes * 60;
+    return this.http.get<LynEvent[]>(`https://lyn.met.no/fetch/1656336591/${sec}`).pipe(
+      map(list => {
+        return list.map(lyn => ({...lyn, Epoch: new Date(lyn.Epoch)}))
+      })
+    );
+  }
 
+
+}
+
+
+export interface LynHistoryResponse {
+
+}
+
+export interface LynDetails extends LynEvent {
+  // "Epoch":"2022-06-27T13:30:06.448702464Z","Point":[20.5903,68.6687],"CloudIndicator":1,"PeakCurrentEstimate":2,"Multiplicity":0,"SolutionNOfSensors":3,"LocationDegreesOfFreedom":3,"EllipseAngle":60.92,"EllipseSemiMajorAxis":0.4,"EllipseSemiMinorAxis":0.4,"ChiSquare":0.55,"RiseTime":6.8,"PeakToZeroTime":2.4,"MaxRateOfRise":1.3,"AngleIndicator":1,"SignalIndicator":0,"TimingIndicator":1
 }
